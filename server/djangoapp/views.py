c7e844c2d84f2b6479b15ebbe6a5f835761ced0f
Edward Isaboke <edward.isaboke@gmail.com>
2026-08-18 16:08:08 -0400

    Update logout

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@
 # from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 # from django.shortcuts import get_object_or_404, render, redirect
-from django.contrib.auth import logout
+# from django.contrib.auth import logout
 # from django.contrib import messages
 # from datetime import datetime
 
@@ -41,7 +41,6 @@ def login_user(request):
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    logout(request)
     data = {"userName": ""}
     return JsonResponse(data)
 
