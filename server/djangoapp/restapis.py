be7f5a787c53c62b98b6cd33b3091bba2b9f8a0c
Edward Isaboke <edward.isaboke@gmail.com>
2026-08-19 06:32:55 -0400

    Search cars frontend

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -63,3 +63,21 @@ def post_review(data_dict):
         print("Network exception occurred")
 
 
+def searchcars_request(endpoint, **kwargs):
+    params = ""
+    if (kwargs):
+        for key, value in kwargs.items():
+            params = params+key + "=" + value + "&"
+
+    request_url = searchcars_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    finally:
+        print("GET request call complete!")
--- server/frontend/src/components/Dealers/Dealer.jsx
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -59,33 +59,32 @@ const Dealer = () => {
     get_dealer();
     get_reviews();
     if(sessionStorage.getItem("username")) {
-      setPostReview(<a href={post_review}><img src={review_icon} style={{width:'10%',marginLeft:'10px',marginTop:'10px'}} alt='Post Review'/></a>)
-
-      
+      setPostReview(<a href={post_review}><img src={review_icon} style={{width:'10%',marginLeft:'10px',marginTop:'10px'}} alt='Post Review'/></a>)      
     }
   },[]);  
 
 
 return(
-  <div style={{margin:"20px"}}>
-      <Header/>
-      <div style={{marginTop:"10px"}}>
-      <h1 style={{color:"grey"}}>{dealer.full_name}{postReview}</h1>
-      <h4  style={{color:"grey"}}>{dealer['city']},{dealer['address']}, Zip - {dealer['zip']}, {dealer['state']} </h4>
-      </div>
-      <div class="reviews_panel">
-      {reviews.length === 0 && unreviewed === false ? (
-        <text>Loading Reviews....</text>
-      ):  unreviewed === true? <div>No reviews yet! </div> :
-      reviews.map(review => (
-        <div className='review_panel'>
-          <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment'/>
-          <div className='review'>{review.review}</div>
-          <div className="reviewer">{review.name} {review.car_make} {review.car_model} {review.car_year}</div>
+    <div style={{margin:"20px"}}>
+        <Header/>
+        <div style={{marginTop:"10px"}}>
+            <h1 style={{color:"grey"}}>{dealer.full_name}{postReview}</h1>
+            <h4  style={{color:"grey"}}>{dealer['city']},{dealer['address']}, Zip - {dealer['zip']}, {dealer['state']} </h4>
+            <a href={`/searchcars/${id}`}>Search Cars</a>	
         </div>
-      ))}
-    </div>  
-  </div>
+        <div class="reviews_panel">
+            {reviews.length === 0 && unreviewed === false ? (
+            <text>Loading Reviews....</text>
+            ):  unreviewed === true? <div>No reviews yet! </div> :
+            reviews.map(review => (
+            <div className='review_panel'>
+                <img src={senti_icon(review.sentiment)} className="emotion_icon" alt='Sentiment'/>
+                <div className='review'>{review.review}</div>
+                <div className="reviewer">{review.name} {review.car_make} {review.car_model} {review.car_year}</div>
+            </div>
+            ))}
+        </div>  
+    </div>
 )
 }
 
