d7237471aae0e2509e6a4e13b54ec189e65d8593
Edward Isaboke <56870745+e-nyakeruma@users.noreply.github.com>
2026-08-19 15:48:15 +0300

    Format import statements in views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,10 +14,10 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from .models import CarMake, CarModel
 from .populate import initiate
-from .restapis import(
-    get_request, 
-    analyze_review_sentiments, 
-    post_review, 
+from .restapis import (
+    get_request,
+    analyze_review_sentiments,
+    post_review,
     searchcars_request
 )
 
